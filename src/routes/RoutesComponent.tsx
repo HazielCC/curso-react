0756617ffb9046fac14df32d1a58debfe7c1257e
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-05 10:20:09 -0700

    Update route paths in RoutesComponent to use centralized paths module

--- src/routes/RoutesComponent.tsx
+++ src/routes/RoutesComponent.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import { ErrorPage } from "../views/error/ErrorPage";
 import {MenuView} from "../views/MenuView.tsx";
 import {PokemonView} from "../views/pokemon/PokemonView.tsx";
+import {paths} from "./paths.tsx";
 
 export const RoutesComponent = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<MenuView/>}/>
-                <Route path="/pokemon-view" element={<PokemonView/>}/>
+                <Route path={paths.home} element={<MenuView/>}/>
+                <Route path={paths.pokemonView} element={<PokemonView/>}/>
 
                 {/*<Route path="/adding-items-page" element={<ExampleCards/>}/>*/}
                 {/*<Route path="/login" element={<LoginPage/>}/>*/}
