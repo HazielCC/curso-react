0882bedf895395fdf844adb8111719f166157a24
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-05 10:37:23 -0700

    Implement PokemonList and integrate with PokemonView to display Pokémon list

--- src/views/pokemon/PokemonView.tsx
+++ src/views/pokemon/PokemonView.tsx
@@ -1,5 +1,21 @@
+import {useContext, useEffect} from "react";
+import PokemonContext from "../../context/pokemon/PokemonContext.tsx";
+import {PokemonList} from "./components/PokemonList.tsx";
+
 export const PokemonView = () => {
+    const {getPokemons, pokemons} = useContext(PokemonContext);
+
+    useEffect(() => {
+        getPokemons().catch(null);
+    }, [getPokemons]);
+
+
     return (
-        <></>
+        <div>
+            <h1>Pokémon</h1>
+
+            {/* Se muestra la lista de Pokemons en pantalla */}
+            <PokemonList pokemons={pokemons ?? []}/>
+        </div>
     );
 };
--- src/views/pokemon/components/PokemonList.tsx
+++ src/views/pokemon/components/PokemonList.tsx
@@ -1,5 +1,22 @@
-export const PokemonList = () => {
+import {Pokemon} from "../../../interfaces/pokemon/PokemonInterfaces";
+import {PokemonSeeDetailsButton} from "./PokemonSeeDetailsButton.tsx";
+
+export const PokemonList = ({pokemons}: { pokemons: Pokemon[] }) => {
+    console.log(pokemons);
     return (
-        <></>
+        <div>
+            <h2>
+                Lista de Pokémons
+            </h2>
+
+            <div>
+                {pokemons.map((pokemon, index) => (
+                        <PokemonSeeDetailsButton key={index} name={pokemon.name} url={pokemon.url}/>
+                    )
+                )}
+            </div>
+
+
+        </div>
     );
 };
