806cac49a8e869d1c503312042ffaa1ca69b26d4
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-09 13:04:18 -0700

    Refactor App component: remove unnecessary div wrapper around PokemonProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
 import {PokemonProvider} from './context/pokemon/PokemonProvider'
 import {RoutesComponent} from "./routes/RoutesComponent.tsx";
 
 function App() {
     return (
-        <div className="App">
-            <PokemonProvider>
-                <RoutesComponent/>
-            </PokemonProvider>
-        </div>
+        <PokemonProvider>
+            <RoutesComponent/>
+        </PokemonProvider>
     )
 }
 
