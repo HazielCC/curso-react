fdc79d74304a18746d49630acacba3c1d6e80d1b
Haziel <haziel.covarrubias@outlook.com>
2025-05-12 12:10:42 -0700

    Refactor MovieView layout and update CSS styles for improved responsiveness

--- src/views/Movies/MovieView.tsx
+++ src/views/Movies/MovieView.tsx
@@ -1,18 +1,45 @@
-import {Box, Grid} from "@mui/material";
-import "../../css/images.css";
+import Grid from "@mui/material/Grid";
+import {Box, Input, InputLabel, TextField} from "@mui/material";
+import cinema from "../../assets/cinema.webp";
+import "../../styles/images.css"
 
 export const MovieView = () => {
     return (
-        <Grid container spacing={2} sx={{height: "100vh", width: "100%"}}>
-            {/* Primera sección - 30% en escritorio, 100% en móvil */}
-            <Grid item xs={2} sm={3}>
-                <Box sx={{backgroundColor: "lightgray", padding: 2, height: "100%", width: "100%"}}>Sección 1</Box>
-            </Grid>
+        <Box sx={{
+            width: "100vw",
+            height: "100vh",
+            overflow: "hidden",
+        }}>
+            <Grid container sx={{
+                height: "100%",
+                display: "flex",
+            }}>
+                <Grid
+                    size={{xs: 12, md: 4}}
+                    sx={{
+                        border: "1px solid red",
+                        height: {xs: "30%", md: "100%"},
+                    }}
+                >
+                    <img src={cinema} className={"image"} alt={"imagen de un cine"}/>
+                </Grid>
+                <Grid
+                    size={{xs: 12, md: 8}}
+                    sx=
+                        {{
+                            border: "1px solid blue",
+                            height: {xs: "70%", md: "100%"},
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: "column",
+                        }}
+                >
+                    <h2>Este ocupa el 70%</h2>
+                    <TextField   label="Outlined"> Hola mundo</TextField >
 
-            {/* Segunda sección - 70% en escritorio, 100% en móvil */}
-            <Grid item xs={6} sm={9}>
-                <Box sx={{backgroundColor: "darkgray", padding: 2, height: "100%", width: "100%"}}>Sección 2</Box>
+                </Grid>
             </Grid>
-        </Grid>
+        </Box>
     );
 };
