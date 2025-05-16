18c2c6257521a76a8c477f87da0595b3f26971d9
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-15 20:34:35 -0700

    Refactor SliderMovies component to simplify layout and improve styling

--- src/views/Movies/components/SliderMovies.tsx
+++ src/views/Movies/components/SliderMovies.tsx
@@ -1,15 +1,9 @@
 import {Result} from "../../../interfaces/Movies/PopularMoviesApi.tsx";
-import {Box, ImageList, ImageListItem, Typography} from "@mui/material";
+import {Box, ImageList, ImageListItem} from "@mui/material";
 
 export const SliderMovies = ({popularMovies}: Readonly<{ popularMovies: Result[] }>) => {
     return (
-        <Box sx={{
-            padding: 2,
-            width: "100%",
-            maxWidth: "1080px",
-            margin: "0 auto",
-        }}>
-            <Typography variant="h2">Tendencia</Typography>
+        <Box>
             <ImageList
                 sx={{
                     display: "flex",
@@ -24,9 +18,10 @@ export const SliderMovies = ({popularMovies}: Readonly<{ popularMovies: Result[]
                             alt={movie.title}
                             style={{
                                 borderRadius: "8px",
+                                height: "auto",
+                                width: "160px"
                             }}
                         />
-                        <Typography variant="subtitle1">{movie.title}</Typography>
                     </ImageListItem>
                 ))}
             </ImageList>
