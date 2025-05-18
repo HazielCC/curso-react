a48e32b586488b0caa66bdf33ea0df2dea60e7cb
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-18 12:15:08 -0700

    Wrap RoutesComponent in Container for improved layout and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {RoutesComponent} from "./routes/RoutesComponent.tsx";
-import {ThemeProvider} from "@mui/material";
+import {Container, ThemeProvider} from "@mui/material";
 import {theme} from "./core/theme/Theme.tsx";
 
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <RoutesComponent/>
+            <Container>
+                <RoutesComponent/>
+            </Container>
         </ThemeProvider>
     )
 }
