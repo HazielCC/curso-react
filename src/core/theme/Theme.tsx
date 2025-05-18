5b7da6b3cbea9670ea5540a3f8efe9f7ea5eddf7
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-05-18 12:14:37 -0700

    Add pokemonTheme with custom typography and spacing for improved styling

--- src/core/theme/Theme.tsx
+++ src/core/theme/Theme.tsx
@@ -7,3 +7,40 @@ export const theme = createTheme({
     typography: CustomTypography,
     palette: CustomPalette,
 });
+
+export const pokemonTheme = createTheme({
+    spacing: 2,
+    typography: {
+        fontFamily: `"8Bit", Arial, sans-serif`,
+        h1: {
+            fontSize: "2rem",
+            fontWeight: 700,
+            lineHeight: 1.5,
+        },
+        h2: {
+            fontSize: "1.5rem",
+            fontWeight: 600,
+            lineHeight: 1.4,
+            padding: "0.5rem 0",
+        },
+        h3: {
+            fontSize: "1.5rem",
+            fontWeight: 500,
+            lineHeight: 1.3,
+            padding: "0.5rem 0",
+        },
+    },
+
+
+    palette: CustomPalette,
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+                @font-face {
+                    font-family: "8Bit";
+                    src: url("/fonts/8_bits.ttf") format("truetype");
+                }
+            `
+        }
+    }
+});
