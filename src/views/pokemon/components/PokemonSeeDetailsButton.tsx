5a12e54d44e388a4c84c1cdbf25e915036d90dc6
Haziel <haziel.covarrubias@outlook.com>
2025-05-19 10:39:37 -0700

    Update padding in PokemonSeeDetailsButton for consistent styling

--- src/views/pokemon/components/PokemonSeeDetailsButton.tsx
+++ src/views/pokemon/components/PokemonSeeDetailsButton.tsx
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom";
 import {Box, Button, Typography} from "@mui/material";
 import {CustomBorderRadius} from "../../../core/constants/customBorderRadius.tsx";
 import {capitalizeFirstLetter} from "../../../core/utilities/InputsUtilities.tsx";
+import {CustomPadding} from "../../../core/constants/customPading.tsx";
 
 export const PokemonSeeDetailsButton = ({name, url}: { name: string, url: string }) => {
     console.log(name, url);
@@ -16,7 +17,7 @@ export const PokemonSeeDetailsButton = ({name, url}: { name: string, url: string
             display: "flex",
             justifyContent: "space-between",
             width: "100%",
-            padding: "0.5rem",
+            padding: CustomPadding.small,
             borderRadius: CustomBorderRadius.small,
             backgroundColor: "#f0f0f0",
             boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
