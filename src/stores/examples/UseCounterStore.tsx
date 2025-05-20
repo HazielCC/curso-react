b94c7fefc99998b01b6ea602674dbb35e338fd09
Haziel <haziel.covarrubias@outlook.com>
2025-05-20 15:00:02 -0700

    Add CounterView component and UseCounterStore for counter functionality

---
+++ src/stores/examples/UseCounterStore.tsx
@@ -0,0 +1,42 @@
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
+
+interface UseCounterStore {
+    count: number;
+    listCount: number[];
+    incrementCount: () => void;
+    decrementCount: () => void;
+    resetList: () => void;
+}
+
+export const UseCounterStore = create<UseCounterStore>()(
+    persist(
+        (set) => ({
+            count: 0,
+            decrementCount: () => {
+                set((state) => {
+                    const newCount = state.count - 1; // Decrement the count
+                    const newListCount = [...state.listCount, newCount]; // Add the new count to the list
+                    return {count: newCount, listCount: newListCount}; // Update the state with the new count and list
+                })
+            },
+            incrementCount: () => {
+                set((state) => {
+                    const newCount = state.count + 1; // Increment the count
+                    const newListCount = [...state.listCount, newCount]; // Add the new count to the list
+                    return {count: newCount, listCount: newListCount}; // Update the state with the new count and list
+
+                });
+            },
+            listCount: [],
+            resetList: () => {
+                set(() => ({
+                    count: 0, // Reset the list to an empty array
+                }));
+            },
+        }),
+        {
+            name: 'position-storage',
+        }, // (optional) by default, 'localStorage' is used
+    ),
+)
---
+++ src/views/examples/CounterView.tsx
@@ -0,0 +1,38 @@
+import {Box, Button, Typography} from "@mui/material";
+import {UseCounterStore} from "../../stores/examples/UseCounterStore";
+
+export const CounterView = () => {
+    // This is a simple counter-example
+    const {count, incrementCount, decrementCount, resetList} = UseCounterStore();
+
+
+    function handleIncrement(): void {
+        incrementCount()
+    }
+
+    const handleReset = () => {
+        resetList()
+    }
+
+    const handleDecrement = () => {
+        decrementCount()
+    }
+
+    return (
+        <Box>
+            <Typography variant={"h1"}>Counter</Typography>
+            <Typography>Counter example</Typography>
+            <Typography>Click the button to increment the counter.</Typography>
+            <Button onClick={handleIncrement}>Increment</Button>
+            <Typography>
+                Click the button to decrement the counter.
+            </Typography>
+            <Button onClick={handleDecrement}>Decrement</Button>
+            <Typography>Click the button to reset the counter.</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+            <Typography>
+                Counter: {count}
+            </Typography>
+        </Box>
+    );
+};
