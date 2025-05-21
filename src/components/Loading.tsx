27e87c9d723023d6ec7b6b9b3f348b579566ce3c
Haziel <haziel.covarrubias@outlook.com>
2025-05-21 10:43:52 -0700

    Refactor Loading component to use MUI Box and Typography, and add MoonLoader for loading indication

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,25 @@
+import {Box, Typography} from "@mui/material";
+import {theme} from "../core/theme/Theme.tsx";
+import {MoonLoader} from "react-spinners";
+
 export const Loading = ({message}: { message: string }) => {
     // Se puede usar un mensaje para mostrar el estado de carga
     return (
-        <div>
-            <h2>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Typography variant="h2">
                 {message}
-            </h2>
-        </div>
+            </Typography>
+            <MoonLoader
+                color={theme.palette.primary.main}
+                size={"60"}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </Box>
     );
 };
